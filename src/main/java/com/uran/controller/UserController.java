28a51d2a924c291e482550bbea9e24aabe85f056
IOURI NEIKOV <neikov@gmail.com>
2016-08-08 19:57:57 +0300

    --

---
+++ src/main/java/com/uran/controller/UserController.java
@@ -0,0 +1,72 @@
+package com.uran.controller;
+
+import com.uran.model.User;
+import com.uran.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/add", method= RequestMethod.GET)
+    public ModelAndView addUserPage() {
+        ModelAndView modelAndView = new ModelAndView("add-user-form");
+        modelAndView.addObject("team", new User());
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/add", method=RequestMethod.POST)
+    public ModelAndView addingTeam(@ModelAttribute User user) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        userService.addUser(user);
+        String message = "User was successfully added.";
+        modelAndView.addObject("message", message);
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/list")
+    public ModelAndView listOfUsers() {
+        ModelAndView modelAndView = new ModelAndView("list-of-teams");
+        List<User> users = userService.getUsers();
+        modelAndView.addObject("users", users);
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
+    public ModelAndView editUserPage(@PathVariable Integer id) {
+        ModelAndView modelAndView = new ModelAndView("edit-user-form");
+        User user = userService.getUser(id);
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/edit/{id}", method=RequestMethod.POST)
+    public ModelAndView edditingUser(@ModelAttribute User user, @PathVariable Integer id) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        userService.updateUser(user);
+        String message = "User was successfully edited.";
+        modelAndView.addObject("message", message);
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+    public ModelAndView deleteUser(@PathVariable Integer id) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        userService.deleteUser(id);
+        String message = "User was successfully deleted.";
+        modelAndView.addObject("message", message);
+        return modelAndView;
+    }
+
+}
