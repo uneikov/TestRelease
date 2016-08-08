e2b02e5e013f59167334d2590876dd8bf5f169e8
IOURI NEIKOV <neikov@gmail.com>
2016-08-08 19:44:56 +0300

    --

---
+++ src/main/java/com/uran/service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.uran.service;
+
+import com.uran.dao.UserDAO;
+import com.uran.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public void addUser(User user) {
+        userDAO.addUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDAO.updateUser(user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return userDAO.getUser(id);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userDAO.getUsers();
+    }
+}
