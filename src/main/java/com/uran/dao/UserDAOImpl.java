1165ef20469c2f78cb88e2ce32b3838b7fa50548
IOURI NEIKOV <neikov@gmail.com>
2016-08-13 08:47:46 +0300

    new commit

--- src/main/java/com/uran/dao/UserDAOImpl.java
+++ src/main/java/com/uran/dao/UserDAOImpl.java
@@ -1,11 +1,14 @@
 package com.uran.dao;
 
 import com.uran.model.User;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
 import org.springframework.stereotype.Repository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -45,5 +48,27 @@ public class UserDAOImpl implements UserDAO {
     @SuppressWarnings("unchecked")
     public List<User> getUsers() {
         return getCurrentSession().createQuery("from User").list();
+    }
+
+    @Transactional
+    public void indexUsers() throws Exception
+    {
+        FullTextSession fullTextSession = Search.getFullTextSession(getCurrentSession());
+        fullTextSession.createIndexer().startAndWait();
+    }
+
+    @Transactional
+    //@SuppressWarnings("unchecked")
+    public List<User> searchUser(String searchText) throws Exception
+    {
+
+        FullTextSession fullTextSession = Search.getFullTextSession(getCurrentSession());
+
+        QueryBuilder qb = fullTextSession.getSearchFactory().buildQueryBuilder().forEntity(User.class).get();
+        org.apache.lucene.search.Query query = qb.keyword().onFields("name").matching(searchText).createQuery();
+
+        //fullTextSession.createFullTextQuery(query, User.class);
+
+        return fullTextSession.createFullTextQuery(query, User.class).list();
     }
 }
--- src/main/java/com/uran/service/UserServiceImpl.java
+++ src/main/java/com/uran/service/UserServiceImpl.java
@@ -19,27 +19,22 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDAO userDAO;
 
-    @Override
     public void addUser(User user) {
         userDAO.addUser(user);
     }
 
-    @Override
     public void updateUser(User user) {
         userDAO.updateUser(user);
     }
 
-    @Override
     public User getUser(int id) {
         return userDAO.getUser(id);
     }
 
-    @Override
     public void deleteUser(int id) {
         userDAO.deleteUser(id);
     }
 
-    @Override
     public List<User> getUsers() {
         return userDAO.getUsers();
     }
